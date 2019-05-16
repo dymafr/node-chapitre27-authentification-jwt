9775c2c4f8972edd9d623437e48bda0b8c5edeed
hades <quentindevial19@gmail.com>
2019-05-16 20:42:18 +0200

    refresh token

--- config/jwt.config.js
+++ config/jwt.config.js
@@ -3,21 +3,36 @@ const jwt = require('jsonwebtoken');
 const { findUserPerId } = require('../queries/user.queries');
 const { app } = require('../app');
 
-const createJwtToken = (user) => {
+const createJwtToken = ({ user = null, id = null }) => {
   const jwtToken = jwt.sign({ 
-    sub: user._id.toString(),
-    exp: Math.floor(Date.now() / 1000) + 1
+    sub: id || user._id.toString(),
+    exp: Math.floor(Date.now() / 1000) + 5 
   }, secret);
   return jwtToken;
 }
 
 exports.createJwtToken = createJwtToken;
 
+const checkExpirationToken = (token, res) => {
+  const tokenExp = token.exp;
+  const nowInSec = Math.floor(Date.now() / 1000);
+  if (nowInSec <= tokenExp) {
+    return token
+  } else if (nowInSec > tokenExp && ((nowInSec - tokenExp) < 60 * 60 * 24) ) {
+    const refreshedToken = createJwtToken({ id: token.sub });
+    res.cookie('jwt', refreshedToken);
+    return jwt.verify(refreshedToken, secret)
+  } else {
+    throw new Error('token expired');
+  }
+}
+
 const extractUserFromToken = async (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
     try {
-      const decodedToken = jwt.verify(token, secret);
+      let decodedToken = jwt.verify(token, secret, { ignoreExpiration: true });
+      decodedToken = checkExpirationToken(decodedToken, res);
       const user = await findUserPerId(decodedToken.sub);
       if (user) {
         req.user = user;
@@ -39,7 +54,7 @@ const addJwtFeatures = (req, res, next) => {
   req.isAuthenticated = () => !!req.user;
   req.logout = () => res.clearCookie('jwt')
   req.login = (user) => {
-    const token = createJwtToken(user);
+    const token = createJwtToken({ user });
     res.cookie('jwt', token);
   }
   next();
