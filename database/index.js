942cd5ede7ac6b875d691aa8ba5fb458f608d530
dymafr <contact@dyma.fr>
2021-03-27 14:54:27 +0100

    maj

--- database/index.js
+++ database/index.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true', {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-}).then( () => {
-  console.log('connexion ok !');
-}).catch( err => {
-  console.log(err);
-})
+mongoose
+  .connect(
+    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true',
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('connexion ok !');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
